f33b3e4d6396222d587628ab5d317c29f68441ad
YIYUN05s2 <61536160+YIYUN05s2@users.noreply.github.com>
2020-03-19 00:07:19 +0800

    Add files via upload

---
+++ YIYUN05s2_資管營.php
@@ -0,0 +1,14 @@
+<form action="submit.php" method="POST">
+	Please input your name:<input type="name=" required=""><br/>
+	Please select T-shirt color:<input type="color" required=""><br/>
+	Please select the birth year month date:<input type="date" required=""><br/>
+	Please enter your email address:<input type="email" required=""><br/>
+Please enter your phone:<input type="phone" required=""><br/>	
+Please select the 出國調查表:<input type="file" required=""><br/>
+	Please select your gender:<br/>
+	Male<input type=radio name="gender" value="M" required="">
+	Female<input type=radio name="gender" value="F" required=""><br/>
+	<input type="reset" value="Clear"><input type="submit" value="Send Data"><br/>
+
+	
+</form>
